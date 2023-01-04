a72d89be76fef39564d190c4a3148b83ac3e2439
Lê Tùng Lâm <86640875+lam-letung@users.noreply.github.com>
2023-01-04 10:12:18 +0700

    save up

---
+++ pages/posts/[...params].tsx
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router';
+import * as React from 'react';
+
+export interface ParamsPageProps {
+}
+
+export default function ParamsPage (props: ParamsPageProps) {
+    const router = useRouter();
+
+
+
+    return (
+    <div>
+     <h1> Post Params Page</h1>
+        <p>Query: {JSON.stringify(router.query)}</p>
+    </div>
+  );
+}
